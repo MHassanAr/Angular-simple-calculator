26e684caf3a600df2719fa390cf3df8a77129a53
MHassanAr <mhassana2344@gmail.com>
2024-10-17 22:51:58 +0500

    Add login of the calculator

--- calculator/src/app/app.component.ts
+++ calculator/src/app/app.component.ts
@@ -1,13 +1,52 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'calculator';
+  result: string = '';
+  keys: string[] = [
+    '7', '8', '9', 'DEL',
+    '4', '5', '6', '+',
+    '1', '2', '3', '-',
+    '.', '0', '/', 'x'
+  ];
+
+  //Handling key presses
+  onKeyPress(key: string) {
+    if (key == 'DEL') {
+      this.result = this.deleteLast(this.result);
+    } else if (key === '=') {
+      this.result = this.evaluateExpression();
+    } else {
+      this.result += key === 'x' ? '*' : key;
+    }
+  }
+
+  //Evaluate the expression function
+  evaluateExpression(): string {
+    try {
+      return Function('"use strict";return (' + this.result + ')')().toString();
+    } catch {
+      return 'Error'; 
+    }
+  }
+
+  //Delete the last character function
+  deleteLast(value: string): string {
+    return value.length > 1 ? value.slice(0, -1) : '0'; 
+  }
+
+  //Reset the calculator function
+  reset() {
+    this.result = '';
+  }
+
+
 }
